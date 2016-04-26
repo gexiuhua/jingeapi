850d036d167f65a90698305a604802d49705b8d7
gexiuhua <gexiuhua@hualongxiang.com>
2016-04-26 17:22:34 +0800

    呼呼

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -49,11 +49,50 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return 'Hello Guys';
+        return $this->render('index');
     }
-	
-	//
-	public function actionError(){
-		throw new \Exception(404);
-	}
+
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+
+    public function actionSay($message = 'Hello'){
+        return $this->render('say',['message'=>$message]);
+    }
 }
